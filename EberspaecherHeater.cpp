ea96fd695963b28ac96e2bec40c79d0fff53c80f
Michael Neuweiler <michael_neuweiler@yahoo.com>
2015-09-22 15:49:24 +0200

    refactor, fix SW-CAN wakeup handling

--- EberspaecherHeater.cpp
+++ EberspaecherHeater.cpp
@@ -26,89 +26,141 @@
 
 #include "EberspaecherHeater.h"
 
+/**
+ * Constructor to initialize class variables
+ */
 EberspaecherHeater::EberspaecherHeater() : Device()
 {
-    canHandlerCar = CanHandler::getInstanceCar();
+    powerRequested = 0;
     commonName = "Eberspaecher Heater";
 }
 
+/**
+ * set-up the device
+ */
 void EberspaecherHeater::setup()
 {
-    TickHandler::getInstance()->detach(this);
-
     Device::setup(); //call base class
 
-    sendWakeup();
+    pinMode(CFG_CAN1_HV_MODE_PIN, OUTPUT);
+    digitalWrite(CFG_CAN1_HV_MODE_PIN, HIGH);
+
     prepareFrames();
+    ready = true;
 
-    // register ourselves as observer of 0x... and 0x... can frames
-    // canHandlerCar->attach(this, CAN_MASKED_ID_1, CAN_MASK_1, false);
-    // canHandlerCar->attach(this, CAN_MASKED_ID_2, CAN_MASK_2, false);
-
-    TickHandler::getInstance()->attach(this, CFG_TICK_INTERVAL_EBERSPAECHER_HEATER);
+    canHandlerCar.attach(this, CAN_MASKED_ID, CAN_MASK, false);
+    tickHandler.attach(this, CFG_TICK_INTERVAL_EBERSPAECHER_HEATER);
 }
 
-void EberspaecherHeater::sendWakeup()
+/**
+ * Tear down the device in a safe way.
+ */
+void EberspaecherHeater::tearDown()
 {
-    // 0x100, False, 0, 00,00,00,00,00,00,00,00
-    canHandlerCar->prepareOutputFrame(&frameControl, CAN_ID_WAKEUP);
-    canHandlerCar->sendFrame(frameControl);
+    Device::tearDown();
+
+    powerRequested = 0;
+    sendControl();
+
+    canHandlerEv.detach(this, CAN_MASKED_ID, CAN_MASK);
 }
 
-void EberspaecherHeater::prepareFrames()
-{
-    // 0x621, False, 8, 00,40,00,00,00,00,00,00 - keep alive
-    canHandlerCar->prepareOutputFrame(&frameKeepAlive, CAN_ID_KEEP_ALIVE);
-    frameKeepAlive.length = 8;
-    frameKeepAlive.data.byte[1] = 0x40;
-    // 0x13FFE060, True, 0, 00,00,00,00,00,00,00,00 - cmd1
-    canHandlerCar->prepareOutputFrame(&frameCmd1, CAN_ID_CMD1);
-    frameCmd1.extended = 1;
-    // 0x102CC040, True, 8, 01,01,CF,0F,00,51,46,60 - cmd2
-    canHandlerCar->prepareOutputFrame(&frameCmd2, CAN_ID_CMD2);
-    frameCmd2.extended = 1;
-    frameCmd2.length = 8;
-    frameCmd2.data.value = 0x0101CF0F00514660;
-    // 0x10242040, True, 1, 00,00,00,00,00,00,00,00 - cmd3
-    canHandlerCar->prepareOutputFrame(&frameCmd3, CAN_ID_CMD3);
-    frameCmd3.extended = 1;
-    frameCmd3.length = 1;
-    // 0x102740CB, True, 3, 2D,00,00,00,00,00,00,00 - cmd4
-    canHandlerCar->prepareOutputFrame(&frameCmd4, CAN_ID_CMD4);
-    frameCmd4.extended = 1;
-    frameCmd4.length = 3;
-    frameCmd4.data.byte[0] = 0x2d;
-    // 0x102740CB, True, 3, 19,00,00,00,00,00,00,00 - cmd5
-    canHandlerCar->prepareOutputFrame(&frameCmd5, CAN_ID_CMD5);
-    frameCmd5.extended = 1;
-    frameCmd5.length = 3;
-    frameCmd5.data.byte[0] = 0x19;
-    // 0x10720099, True, 5, 02,3E,00,00,00,00,00,00 - control
-    canHandlerCar->prepareOutputFrame(&frameControl, CAN_ID_CONTROL);
-    frameControl.extended = 1;
-    frameControl.length = 5;
-    frameControl.data.byte[0] = 0x02;
-}
-
+/**
+ * process a tick event from the timer the device is registered to.
+ */
 void EberspaecherHeater::handleTick()
 {
+    calculatePower();
     sendControl();
 }
 
 /*
  * Processes an event from the CanHandler.
- *
- * In case a CAN message was received which pass the masking and id filtering,
- * this method is called. Depending on the ID of the CAN message, the data of
- * the incoming message is processed.
  */
 void EberspaecherHeater::handleCanFrame(CAN_FRAME *frame)
 {
+    canHandlerCar.logFrame(*frame);
+
     switch (frame->id) {
     case CAN_ID_STATUS:
         processStatus(frame->data.bytes);
         break;
     }
+}
+
+/*
+ * Wake up all SW-CAN devices by switching the transceiver to HV mode and
+ * sending the command 0x100 and switching the HV mode off again.
+ */
+void EberspaecherHeater::sendWakeup()
+{
+    digitalWrite(CFG_CAN1_HV_MODE_PIN, LOW); // set HV mode
+
+    // 0x100, False, 0, 00,00,00,00,00,00,00,00
+    canHandlerCar.prepareOutputFrame(&frameControl, CAN_ID_WAKEUP);
+    canHandlerCar.sendFrame(frameControl);
+    delay(5);
+
+    digitalWrite(CFG_CAN1_HV_MODE_PIN, HIGH); // set normal mode
+}
+
+/*
+ * Pre-fill all required CAN frames
+ */
+void EberspaecherHeater::prepareFrames()
+{
+    // 0x621, False, 8, 00,40,00,00,00,00,00,00 - keep alive
+    canHandlerCar.prepareOutputFrame(&frameKeepAlive, CAN_ID_KEEP_ALIVE);
+    frameKeepAlive.length = 8;
+    frameKeepAlive.data.byte[1] = 0x40;
+    // 0x13FFE060, True, 0, 00,00,00,00,00,00,00,00 - cmd1
+    canHandlerCar.prepareOutputFrame(&frameCmd1, CAN_ID_CMD1);
+    frameCmd1.extended = 1;
+    // 0x102CC040, True, 8, 01,01,CF,0F,00,51,46,60 - cmd2
+    canHandlerCar.prepareOutputFrame(&frameCmd2, CAN_ID_CMD2);
+    frameCmd2.extended = 1;
+    frameCmd2.length = 8;
+    frameCmd2.data.value = 0x0101CF0F00514660;
+    // 0x10242040, True, 1, 00,00,00,00,00,00,00,00 - cmd3
+    canHandlerCar.prepareOutputFrame(&frameCmd3, CAN_ID_CMD3);
+    frameCmd3.extended = 1;
+    frameCmd3.length = 1;
+    // 0x102740CB, True, 3, 2D,00,00,00,00,00,00,00 - cmd4
+    canHandlerCar.prepareOutputFrame(&frameCmd4, CAN_ID_CMD4);
+    frameCmd4.extended = 1;
+    frameCmd4.length = 3;
+    frameCmd4.data.byte[0] = 0x2d;
+    // 0x102740CB, True, 3, 19,00,00,00,00,00,00,00 - cmd5
+    canHandlerCar.prepareOutputFrame(&frameCmd5, CAN_ID_CMD5);
+    frameCmd5.extended = 1;
+    frameCmd5.length = 3;
+    frameCmd5.data.byte[0] = 0x19;
+    // 0x10720099, True, 5, 02,3E,00,00,00,00,00,00 - control
+    canHandlerCar.prepareOutputFrame(&frameControl, CAN_ID_CONTROL);
+    frameControl.extended = 1;
+    frameControl.length = 5;
+    frameControl.data.byte[0] = 0x02;
+}
+
+/*
+ * Calculate the desired output power based on measured temperature.
+ */
+void EberspaecherHeater::calculatePower()
+{
+    powerRequested = 0; // value from 0 to 100 percent power
+
+    //TODO: read temperature via analog input. based on delta to configured temperature set the power requested
+    powerRequested = 5;
+
+    if (powerRequested > 0) {
+        if (!running) {
+            sendWakeup();
+        }
+        running = true;
+    } else {
+        running = false;
+    }
+    running = (powerRequested > 0 ? true : false);
 }
 
 /*
@@ -118,33 +170,30 @@ void EberspaecherHeater::handleCanFrame(CAN_FRAME *frame)
  */
 void EberspaecherHeater::sendControl()
 {
-    uint8_t powerRequested = 0; // value from 0 to 100 percent power
-
     frameControl.data.byte[1] = map(powerRequested, 0, 100, 0, 0x85);
 
     if (Logger::isDebug()) {
         Logger::debug(EBERSPAECHER, "requested power: %l", powerRequested);
-        canHandlerCar->logFrame(frameKeepAlive);
-        canHandlerCar->logFrame(frameCmd1);
-        canHandlerCar->logFrame(frameControl);
-        canHandlerCar->logFrame(frameCmd2);
-        canHandlerCar->logFrame(frameCmd3);
-        canHandlerCar->logFrame(frameCmd4);
-        canHandlerCar->logFrame(frameCmd5);
+        canHandlerCar.logFrame(frameKeepAlive);
+        canHandlerCar.logFrame(frameCmd1);
+        canHandlerCar.logFrame(frameControl);
+        canHandlerCar.logFrame(frameCmd2);
+        canHandlerCar.logFrame(frameCmd3);
+        canHandlerCar.logFrame(frameCmd4);
+        canHandlerCar.logFrame(frameCmd5);
     }
 
-    canHandlerCar->sendFrame(frameKeepAlive);
-    canHandlerCar->sendFrame(frameCmd1);
-    canHandlerCar->sendFrame(frameControl);
-    canHandlerCar->sendFrame(frameCmd2);
-    canHandlerCar->sendFrame(frameCmd3);
-    canHandlerCar->sendFrame(frameCmd4);
-    canHandlerCar->sendFrame(frameCmd5);
+    canHandlerCar.sendFrame(frameKeepAlive);
+    canHandlerCar.sendFrame(frameCmd1);
+    canHandlerCar.sendFrame(frameControl);
+    canHandlerCar.sendFrame(frameCmd2);
+    canHandlerCar.sendFrame(frameCmd3);
+    canHandlerCar.sendFrame(frameCmd4);
+    canHandlerCar.sendFrame(frameCmd5);
 }
 
 /*
  * Process a status message which was received from the heater.
- *
  */
 void EberspaecherHeater::processStatus(uint8_t data[])
 {
@@ -159,4 +208,52 @@ DeviceType EberspaecherHeater::getType()
 DeviceId EberspaecherHeater::getId()
 {
     return EBERSPAECHER;
+}
+
+/*
+ * Load configuration data from EEPROM.
+ *
+ * If not available or the checksum is invalid, default values are chosen.
+ */
+void EberspaecherHeater::loadConfiguration()
+{
+    EberspaecherHeaterConfiguration *config = (EberspaecherHeaterConfiguration *) getConfiguration();
+
+    if (!config) { // as lowest sub-class make sure we have a config object
+        config = new EberspaecherHeaterConfiguration();
+        setConfiguration(config);
+    }
+
+    Device::loadConfiguration(); // call parent
+
+#ifdef USE_HARD_CODED
+
+    if (false) {
+#else
+    if (prefsHandler->checksumValid()) { //checksum is good, read in the values stored in EEPROM
+#endif
+        uint8_t temp;
+        prefsHandler->read(EEHEAT_xxx, &config->xxx);
+        prefsHandler->read(EEHEAT_yyy, &temp);
+        config->yyy = (temp != 0);
+    } else { //checksum invalid, reinitialize values and store to EEPROM
+        config->xxx = 0;
+        config->yyy = false;
+        saveConfiguration();
+    }
+    Logger::info(EBERSPAECHER, "xxx: %d, yyy: %B", config->xxx, config->yyy);
+}
+
+/*
+ * Store the current configuration parameters to EEPROM.
+ */
+void EberspaecherHeater::saveConfiguration()
+{
+    EberspaecherHeaterConfiguration *config = (EberspaecherHeaterConfiguration *) getConfiguration();
+
+    Device::saveConfiguration(); // call parent
+
+    prefsHandler->write(EEHEAT_xxx, config->xxx);
+    prefsHandler->write(EEHEAT_yyy, (uint8_t) (config->yyy ? 1 : 0));
+    prefsHandler->saveChecksum();
 }
--- EberspaecherHeater.h
+++ EberspaecherHeater.h
@@ -15,7 +15,7 @@
 // CAN bus id's for frames sent to the heater
 #define CAN_ID_WAKEUP       0x100 // wake up the device
 #define CAN_ID_KEEP_ALIVE   0x621 // keep alive message
-#define CAN_ID_CONTROL      0x10720099 // send power conmtrol message
+#define CAN_ID_CONTROL      0x10720099 // send power control message
 #define CAN_ID_CMD1 0x13FFE060 // dummy message
 #define CAN_ID_CMD2 0x102CC040 // dummy message
 #define CAN_ID_CMD3 0x10242040 // dummy message
@@ -25,29 +25,34 @@
 // CAN bus id's for frames received from the heater
 
 //TODO: define correct can ID's, mask and masked id's
-#define CAN_ID_STATUS           0x258 // receive status message                  01001011000
-#define CAN_MASK_1              0x7cc // mask for above id's                     11111001100
-#define CAN_MASKED_ID_1         0x248 // masked id for id's from 0x258 to 0x268  01001001000
+#define CAN_ID_STATUS           0x555 // receive status message                  10101010101
+#define CAN_MASK                0x0   // mask for above id's                     00000000000
+#define CAN_MASKED_ID           0x7ff // masked id for id's from 0x258 to 0x268  01001001000
 
-#define CAN_ID_TEMP         0x458 // receive temperature information             10001011000
-#define CAN_MASK_2          0x7ff // mask for above id's                         11111111111
-#define CAN_MASKED_ID_2     0x458 // masked id for id's from 0x258 to 0x268      10001011000
+class EberspaecherHeaterConfiguration : public DeviceConfiguration
+{
+public:
+    uint16_t xxx; // in W
+    bool yyy; // flag ...
+};
 
 class EberspaecherHeater: public Device, CanObserver
 {
 public:
     EberspaecherHeater();
     void setup();
+    void tearDown();
     void handleTick();
     void handleCanFrame(CAN_FRAME *frame);
     void processStatus(uint8_t data[]);
     DeviceId getId();
     DeviceType getType();
+    void loadConfiguration();
+    void saveConfiguration();
 
 protected:
 
 private:
-    CanHandler *canHandlerCar;
     CAN_FRAME frameControl; // frame to send control messages
     CAN_FRAME frameKeepAlive; // frame to send heart beat
     CAN_FRAME frameCmd1; // frame to send cmd1 message
@@ -55,7 +60,9 @@ private:
     CAN_FRAME frameCmd3; // frame to send cmd3 message
     CAN_FRAME frameCmd4; // frame to send cmd4 message
     CAN_FRAME frameCmd5; // frame to send cmd5 message
+    uint8_t powerRequested; // value from 0 to 100 percent power
 
+    void calculatePower();
     void sendControl();
     void sendWakeup();
     void prepareFrames();
